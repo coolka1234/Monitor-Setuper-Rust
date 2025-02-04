d83c9981e576d5708d54f38b709537bf7a56c7a7
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2025-02-04 23:13:23 +0100

    test xrandr

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,6 @@ fn get_monitor_ids()->String{
         .arg("-q")
         .spawn()
         .expect("Failed to execute xrandr query. Make sure you have an up to date xrandr installation.");
-    // let result= match xrandr_query {
-    //     Ok(child) => child,
-    //     Err(_) => Child() 
-    // };
     let Output{stdout,..}=xrandr_query.wait_with_output().unwrap();
     let xrandr_output=String::from_utf8_lossy(&stdout);
     return xrandr_output.to_string();
@@ -30,5 +26,15 @@ fn find_tags_of_monitors(input: String)->String{
     let Output{stdout,..}=greped_lines.wait_with_output().unwrap();
     let tag_lines=String::from_utf8_lossy(&stdout);
     return tag_lines.to_string();
+}
 
+fn test_xrandr(){
+    let xrandr_query=Command::new("/usr/bin/xrandr")
+        .arg("-q")
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Failed to execute xrandr query. Make sure you have an up to date xrandr installation.");
+    let Output{stdout,..}=xrandr_query.wait_with_output().unwrap();
+    let xrandr_output=String::from_utf8_lossy(&stdout);
+    print!("{}",xrandr_output);
 }
